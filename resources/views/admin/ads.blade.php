06bb4c9ac11a5ec58ef9acc5cb800284faa7df1a
ShevtsovTN <51271499+ShevtsovTN@users.noreply.github.com>
2020-05-31 21:42:18 +0200

    Sorting backend

--- resources/views/admin/ads.blade.php
+++ resources/views/admin/ads.blade.php
@@ -8,5 +8,33 @@
             <h1 class="page-header">Forms</h1>
         </div>
     </div><!--/.row-->
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">Listing elements</div>
+                <div class="panel-body">
+                    <div class="col-12">
+                        <form class="col-12" name="listingForm" role="form">
+                            <div class="row">
+                                <div class="form-group">
+                                    <label>Title ES</label>
+                                    <input name="title_es" class="form-control" placeholder="Placeholder">
+                                </div>
+                                <div class="form-group">
+                                    <label>Title EN</label>
+                                    <input name="title_en" class="form-control" placeholder="Placeholder">
+                                </div>
+                                <div class="form-group">
+                                    <label>Title RU</label>
+                                    <input name="title_ru" class="form-control" placeholder="Placeholder">
+                                </div>
 
+                            </div>
+                            <input value="Submit" form="listingForm" type="submit" class="btn btn-primary">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div><!-- /.col-->
+    </div><!-- /.row -->
 @endsection
--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -126,12 +126,12 @@
 </div><!--/.sidebar-->
 
 <div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
-    <div class="row">
-        <!--<ol class="breadcrumb">
+    <!--<div class="row">
+        <ol class="breadcrumb">
             <li><a href="#"><span class="glyphicon glyphicon-home"></span></a></li>
             <li class="active">Listings</li>
-        </ol>-->
-    </div><!--/.row-->
+        </ol>
+    </div>--><!--/.row-->
 
     @yield('admin_content')
 
